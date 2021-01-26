f1007912b031e8fbee8e4fef81e1746a83e1036f
Lyubov Ubushin <Lyubau1124@gmail.com>
2021-01-26 15:24:20 -0500

    movie 1 branch spouce came and changed to B22

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World, B21");
+        System.out.println("Hello World, B22");
         System.out.println("B21 is future SDET");
         //watching more movie in bedroom
 

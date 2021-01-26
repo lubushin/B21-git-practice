8844e6f1c605cd44af30ecadeb6f747cc00af018
Lyubov Ubushin <Lyubau1124@gmail.com>
2021-01-26 15:23:16 -0500

    movie 1 branch more work here

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World, B21");
-
         System.out.println("B21 is future SDET");
+        //watching more movie in bedroom
+
     }
 }

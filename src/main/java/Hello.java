6e5047d9dcc1f7bb17a61fc299517b1e97e881f6
Lyubov Ubushin <Lyubau1124@gmail.com>
2021-01-26 15:39:46 -0500

    master branch watching like movie

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,8 +4,7 @@ public class Hello {
         System.out.println("Hello World B22");
         System.out.println("B21 is future SDET");
         System.out.println("Ready to go back to living room");
-        //movie on the phone
-        //movie on the phone 20 min
+    //oh ilkie this movie
 
     }
 }

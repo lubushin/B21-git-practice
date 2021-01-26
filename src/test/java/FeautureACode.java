b221b9f430cc91beb4a58f19d5c374db90bab1a1
Lyubov Ubushin <Lyubau1124@gmail.com>
2021-01-26 14:23:31 -0500

    more work on featureA on branch

--- src/test/java/FeautureACode.java
+++ src/test/java/FeautureACode.java
@@ -1,5 +1,6 @@
 public class FeautureACode {
     public static void main(String[] args) {
         System.out.println("AWWESOME WORK FOR A STARTED");
+        System.out.println("B21 is learning");
     }
 }

f539ddc33012ed61c6fbcb39c3641141d31d0a63
Lyubov Ubushin <Lyubau1124@gmail.com>
2021-01-26 15:38:05 -0500

    movie 2 branch movie on the phone

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,10 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World, B22");
+        System.out.println("Hello World B22");
         System.out.println("B21 is future SDET");
-        //watching more movie in bedroom
         System.out.println("Ready to go back to living room");
+        //movie on the phone
 
     }
 }

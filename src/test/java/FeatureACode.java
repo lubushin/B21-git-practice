621a583f0b8abab46e325e4c406192453ee05c49
Lyubov Ubushin <Lyubau1124@gmail.com>
2021-01-26 14:40:18 -0500

    Initial work on FeatureCTest

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -1,4 +1,4 @@
-public class FeautureACode {
+public class FeatureACode {
     public static void main(String[] args) {
         System.out.println("AWWESOME WORK FOR A STARTED");
         System.out.println("B21 is learning");

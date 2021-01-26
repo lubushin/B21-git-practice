9742d6d75d961c71b4795440ba7e6d6ea65ad617
Lyubov Ubushin <Lyubau1124@gmail.com>
2021-01-26 16:28:46 -0500

    Master new work started movie5 not know

--- src/main/java/SeeingDifference.java
+++ src/main/java/SeeingDifference.java
@@ -1,4 +1,4 @@
 public class SeeingDifference {
 //this is new line of work in movie
-    //this is new line of work in movie in master
+    //this is new line of work in movie in master, movie5 does not know that
 }

cca56a08fc4a8eae248cce0fa2ee5e609ec59d5f
Lyubov Ubushin <Lyubau1124@gmail.com>
2021-01-26 15:41:07 -0500

    movie2 branch watching another 10 min

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
         System.out.println("B21 is future SDET");
         System.out.println("Ready to go back to living room");
     //oh ilkie this movie
+        //another final hours 10 min
 
     }
 }
